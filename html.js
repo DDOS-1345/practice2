736404b91d0eb05cf2cefe8fdd23f299de6a80bc
DDOS-1345 <112930365+DDOS-1345@users.noreply.github.com>
2023-06-13 17:06:55 +0900

    hjh

---
+++ html.js
@@ -0,0 +1,16 @@
+function bmi(){ 
+  let a, b, d;
+  a = document.getElementById("shincho").value; 
+  b = document.getElementById("taijuu").value;
+  
+  c = b/( (a/100)*(a/100) ); 
+
+ if(c >= 25){ 
+   document.getElementById("kuuran").textContent = "肥満型"; }
+  else if(c < 25 && c >= 18){ 
+   document.getElementById("kuuran").textContent = "標準型"; }
+  else{ 
+    
+     document.getElementById("kuuran").textContent = "瘦せ型";
+ }
+ }
